0b85354a60048e3fd430cb2b2377a99694d1673b
Arrad1 <wayne@arradfootbalconies.com>
2026-09-15 17:44:42 +0100

    Add lib/decking-modules.ts

---
+++ lib/decking-modules.ts
@@ -0,0 +1,40 @@
+export type PostCorner = "tl" | "tr" | "bl" | "br";
+
+export type DeckModule = {
+  id: number;
+  widthFt: number;
+  widthMm: number;
+  lengthFt: 6 | 8;
+  lengthMm: 1850 | 2470;
+  layout: "A" | "B";
+  price: number;
+  posts: PostCorner[];
+};
+
+const widths = [
+  { ft: 3, mm: 870, prices: [867, 612, 709, 984] },
+  { ft: 4, mm: 1170, prices: [978, 724, 840, 1105] },
+  { ft: 5, mm: 1480, prices: [1205, 861, 1013, 1337] },
+  { ft: 6, mm: 1760, prices: [1205, 861, 1013, 1337] },
+  { ft: 7, mm: 2080, prices: [1370, 1011, 1268, 1657] },
+  { ft: 8, mm: 2470, prices: [1370, 1011, 1268, 1657] },
+];
+
+const variants: Array<Pick<DeckModule, "lengthFt" | "lengthMm" | "layout" | "posts">> = [
+  { lengthFt: 6, lengthMm: 1850, layout: "A", posts: ["tl", "tr", "bl", "br"] },
+  { lengthFt: 6, lengthMm: 1850, layout: "B", posts: ["tr", "bl", "br"] },
+  { lengthFt: 8, lengthMm: 2470, layout: "A", posts: ["tr", "bl"] },
+  { lengthFt: 8, lengthMm: 2470, layout: "B", posts: ["tl", "tr", "bl", "br"] },
+];
+
+export const POST_SIZE_MM = 100;
+
+export const deckModules: DeckModule[] = widths.flatMap((width, widthIndex) =>
+  variants.map((variant, variantIndex) => ({
+    id: widthIndex * 4 + variantIndex + 1,
+    widthFt: width.ft,
+    widthMm: width.mm,
+    price: width.prices[variantIndex],
+    ...variant,
+  })),
+);
